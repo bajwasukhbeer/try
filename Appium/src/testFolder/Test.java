ffd0b5629736735ed2306c332b517c457577e175
bajwasukhbeer <bajwa.sukhbeersingh@gmail.com>
2022-03-15 16:21:48 +0530

    first commit on march 15

---
+++ Appium/src/testFolder/Test.java
@@ -0,0 +1,15 @@
+package testFolder;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("testing for Appium");
+		int a =10;
+		int b = 20;
+		int c = a+b;
+		System.out.println("value of C is "+ c);
+	}
+
+}
---
+++ AppiumTut/src/module-info.java
@@ -0,0 +1,2 @@
+module AppiumTut {
+}
